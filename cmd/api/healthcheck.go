efacc5b345b6af8ae25abebd1be13f8a6ac37bcd
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-03-14 13:09:52 +0630

    Custom Error Handling

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,14 +6,15 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	env := envelope{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encounterd a problem and could not process your Request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
