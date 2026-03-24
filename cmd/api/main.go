e094058a97976439ada72d0867f2bbbb75e56d3e
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-03-24 22:18:51 +0630

    Setting up the users database table

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -88,36 +88,6 @@ func main() {
 		logger.Error(err.Error())
 		os.Exit(1)
 	}
-
-	// srv := &http.Server{
-	// 	Addr:         fmt.Sprintf(":%d", cfg.port),
-	// 	Handler:      app.routes(),
-	// 	IdleTimeout:  time.Minute,
-	// 	ReadTimeout:  5 * time.Second,
-	// 	WriteTimeout: 10 * time.Second,
-	// 	ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
-	// }
-	//
-	// go func() {
-	// 	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-	//
-	// 	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-	// 		logger.Error("listen and server error", "error", err)
-	// 		os.Exit(1)
-	// 	}
-	// }()
-	//
-	// <-poolCtx.Done()
-	// logger.Info("Shutting down gracefully...")
-	//
-	// shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	// defer cancel()
-	//
-	// if err := srv.Shutdown(shutdownCtx); err != nil {
-	// 	logger.Error("server forced to shutdown", "error", err)
-	// }
-	//
-	// logger.Info("server stopped")
 }
 
 func createPool(ctx context.Context, cfg config) (*pgxpool.Pool, error) {
