42bf09a7a45787b6a43416543e59971fc4054f48
htethlaingwin <htethlaingwin.bsn29@gmail.com>
2026-04-01 23:22:01 +0630

    Simple CORS requests

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 	"sync"
 	"syscall"
 	"time"
@@ -39,6 +40,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -73,6 +77,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "8fb1dfd82cfaf8", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "7fe470f4c9a2ab", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.htethlaingwin.net>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
